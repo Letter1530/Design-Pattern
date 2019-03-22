ab8233b69c9392fca6b303da6812aaa36dbc2106
stu <stu@192.168.0.103>
2019-03-22 15:05:00 +0800

    update observerP

--- src/observerPattern/Test.java
+++ src/observerPattern/Test.java
@@ -2,8 +2,13 @@ package observerPattern;
 
 public class Test {
 
+	// https://dotblogs.com.tw/joysdw12/2013/03/13/96531
+	// http://twmht.github.io/blog/posts/design-pattern/observer.html
 	public static void main(String[] args) {
 
+		/*
+		 * 觀察者模式定義了物件之間的一對多關係，如此一來，當一個物件改變狀態，其他相依者都會收到通知並自動被更新
+		 */
 		NewOffice office = new NewOffice(); // 建立一個新聞社(Subject)
 
 		Reader john = new Reader("John"); // 建立一些讀者
