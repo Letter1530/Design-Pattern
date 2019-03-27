d07eff4147152fd62a9c82ed08211db787ab6430
fatfat <lf2lf211@hotmail.com>
2019-03-27 15:34:12 +0800

    builderPatternbridgePatternmementoPattern

--- src/builderPattern/before/Client.java
+++ src/builderPattern/before/Client.java
@@ -33,5 +33,21 @@ public class Client {
 		soul.createRightLeg("胖胖的右腿");
 		soul.createRightFoot("大大的右脚");
 		System.out.println("获得了" + soul.getDoppelganger());
+
+		System.out.println("----------------------------------------");
+
+		// 制造一个胖胖的代打工程師2
+		soul.createDoppelganger("代打工程師3");
+		soul.createBody("胖胖的身体");
+		soul.createHead("大大的头");
+		soul.createLeftArm("胖胖的左胳膊");
+		soul.createLeftHand("胖胖的左手");
+		soul.createRightArm("胖胖的右胳膊");
+		soul.createRightHand("胖胖的右手");
+		soul.createLeftLeg("胖胖的左腿");
+		soul.createLeftFoot("大大的左脚");
+		soul.createRightLeg("胖胖的右腿");
+		soul.createRightFoot("大大的右脚");
+		System.out.println("获得了" + soul.getDoppelganger());
 	}
 }
